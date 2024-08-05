7ee246da39a526e099132fc0306f6fdc65d2ab92
Juanxetee <buquerin6@hotmail.com>
2024-08-05 21:08:33 +0200

    .

--- app_model.py
+++ app_model.py
@@ -78,7 +78,7 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
         
         # Dividir en entrenamiento y prueba
         X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-        model = pickle.load(open(path_base + 'ad_model.pkl','rb'))
+        model = pickle.load(open(path_base + 'ad_model.pkl'))
         # Reentrenar el modelo
         model.fit(X_train, y_train)
         
